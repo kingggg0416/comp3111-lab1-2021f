06313477ce9c877a43971ecb7144556365621736
kingggg0416 <kelvin16042002@gmail.com>
2023-02-16 22:11:24 +0800

    Initial project setup

---
+++ lib/src/main/java/Lab1/Library.java
@@ -0,0 +1,12 @@
+package Lab1;
+
+public class Library {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+	
+    public boolean someLibraryMethod() {
+        return true;
+    }
+}
